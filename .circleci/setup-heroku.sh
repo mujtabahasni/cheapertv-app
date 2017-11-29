d072fbfbbe410ede14258d4394482369918c2bc2
Mujtaba Hasni <mujtaba.hasni@rangle.io>
2017-11-29 14:05:25 -0500

    Fixing setup script for heroku.

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -10,7 +10,7 @@ sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_LOGIN
-    password $HEROKU_API_KEY
+  password $HEROKU_API_KEY
 EOF
 
 cat >> ~/.ssh/config << EOF
