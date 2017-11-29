e5a26afe8f0c1a08ef75b3c6808d7dbb73639d4c
Mujtaba Hasni <mujtaba.hasni@rangle.io>
2017-11-29 12:46:20 -0500

    CircleCi 2.0 does not support heroku natively, so I will perform a manual depolyment.
    CircleCi 2.0 does not support heroku natively, so I will perform a manual depolyment.
    
    CircleCi 2.0 does not support heroku natively, so I will perform a manual depolyment.
    
    CircleCi 2.0 does not support heroku natively, so I will perform a manual depolyment.
    
    CircleCi 2.0 does not support heroku natively, so I will perform a manual depolyment.
    
    CircleCi 2.0 does not support heroku natively, so I will perform a manual depolyment.
    
    CircleCi 2.0 does not support heroku natively, so I will perform a manual depolyment.
    
    CircleCi 2.0 does not support heroku natively, so I will perform a manual depolyment.
    
    CircleCi 2.0 does not support heroku natively, so I will perform a manual depolyment.
    
    CircleCi 2.0 does not support heroku natively, so I will perform a manual depolyment.
    
    CircleCi 2.0 does not support heroku natively, so I will perform a manual depolyment.
    
    CircleCi 2.0 does not support heroku natively, so I will perform a manual depolyment.
    
    CircleCi 2.0 does not support heroku natively, so I will perform a manual depolyment.

---
+++ .circleci/setup-heroku.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This file was pasted from: 
+#   https://circleci.com/docs/2.0/deployment_integrations/#heroku
+
+wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+sudo mkdir -p /usr/local/lib /usr/local/bin
+sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_LOGIN
+    password $HEROKU_API_KEY
+EOF
+
+cat >> ~/.ssh/config << EOF
+  VerifyHostKeyDNS yes
+  StrictHostKeyChecking no
+EOF
