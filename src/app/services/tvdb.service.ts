4e3aed7696144cbc3e13b8ac1145fef12db98122
Mujtaba Hasni <mujtaba.hasni@rangle.io>
2017-12-06 11:14:10 -0500

    Now only master branch can deploy to production
    removed logging.  Returns Promise<TvShowDatap[]> instead of
    
    Adding posters to tvshow data and search results.  Summaries are also included in model.
    
    Using test to determine if this branch is master for pusing to production
    
    Fixing typo in config.
    
    Removing unecessary restart of dyno
    
    removed logging.  Returns Promise<TvShowDatap[]> instead of

--- src/app/services/tvdb.service.ts
+++ src/app/services/tvdb.service.ts
@@ -12,14 +12,15 @@ export class TvDbService {
 
   constructor(private http: Http) { }
 
-  search(query: string): Promise<any> {
+  search(query: string): Promise<TvShowData[]> {
     const q = encodeURI(query);
     return this.http
       .get(`${API_BASE_URL}/search/shows?q=${q}`)
       .map((response) => response.json())
-      .do((json) => console.log(json))
       .map((shows) => shows.map((item) => ({
         title: item.show.name,
+        summary: item.show.summary,
+        posterUrl: item.show.image ? item.show.image.medium : null
       })))
       .toPromise();
   }
