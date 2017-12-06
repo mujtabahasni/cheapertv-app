519ee1752b420a552a6db54645dfcbb1e1ef422f
Mujtaba Hasni <mujtaba.hasni@rangle.io>
2017-12-06 10:25:53 -0500

    Adding posters to tvshow data and search results.  Summaries are also included in model.

--- src/app/tvshows/index.ts
+++ src/app/tvshows/index.ts
@@ -1,4 +1,5 @@
 export * from './tvshows-search.component';
+export * from './tvshows-tvdata.component';
 export * from './store/tvshows.reducer';
 export * from './store/tvshows.actions';
 export * from './store/tvshows.selectors';
