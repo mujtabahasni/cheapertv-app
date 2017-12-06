86ca7d8a315acb649210dbec7d3b0f3de46d517e
Mujtaba Hasni <mujtaba.hasni@rangle.io>
2017-12-06 12:28:51 -0500

    Adding TV Posters to search results.  Search results items are now rendered in its own component

--- src/app/tvshows/tvshows-search.component.ts
+++ src/app/tvshows/tvshows-search.component.ts
@@ -17,13 +17,9 @@ import { TvShowData } from './store/tvshows.models';
   <div *ngIf="selectors.isFetching$ | async">
     <progress value="80" max="100">fetching...</progress>
   </div>
-    <table border="1" style="display:inline-block" *ngFor="let show of (selectors.tvshows$ | async)">
-    <tr>
-      <td><h4>{{ show.title }}</h4></td>
-    </tr>
-    <tr>
-     <td><img [src]="show.posterUrl"></td>
-    </tr>
+  <span *ngFor="let show of (selectors.tvshows$ | async)">
+    <app-tvshow-poster [show]="show"></app-tvshow-poster>
+  </span>
   `
 })
 export class TvShowsSearchViewComponent {
